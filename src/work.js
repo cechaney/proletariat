e8013f76b96801b67c94c44811721180289d5ab4
Christopher Chaney <christopher_e_chaney@homedepot.com>
2019-02-06 15:05:11 -0600

    More stuff, still not stable

--- src/work.js
+++ src/work.js
@@ -1,13 +1,8 @@
 const {  parentPort, workerData } = require('worker_threads');
 
 parentPort.on('message', (workItem) => {
-  render(workItem);
-});
-
-function render(workItem){
 
   workItem.output = `<h1>This is rendered markup: ${workItem.data}</h1>`
-
   parentPort.postMessage(workItem);
 
-}
+});
