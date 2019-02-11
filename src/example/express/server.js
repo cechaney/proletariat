9e845ff74618cc120322db04d67308f3e48b7291
Christopher Chaney <christopher_e_chaney@homedepot.com>
2019-02-11 12:36:20 -0600

    Log change

--- src/example/express/server.js
+++ src/example/express/server.js
@@ -23,4 +23,4 @@ app.get('/', (req, res) => {
 
 });
 
-app.listen(port, () => console.log(`Pooled execution app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
